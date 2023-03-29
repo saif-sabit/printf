90b1a955b21d5851851bd2e35740f6919cbabb53
asimsharf <asimsharf@gmail.com>
2023-03-29 23:08:44 +0300

    Handle the following custom conversion specifier

--- pcs.c
+++ pcs.c
@@ -5,56 +5,38 @@
  * @str: list of arguments
  * Return: number of characters printed
  */
-//Handle the following custom conversion specifier:
 
 int pcs(char *str)
 {
-
-
-	int i, j, k;
+	int i = 0;
+	int j = 0;
 	int count = 0;
-	char *hex = "0123456789abcdef";
-	char *hex2 = "0123456789ABCDEF";
+	char *hex = "0123456789ABCDEF";
 
 	if (str == NULL)
 	{
 		return (-1);
 	}
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
 		if (str[i] < 32 || str[i] >= 127)
 		{
 			_putchar('\\');
 			_putchar('x');
-			_putchar('0');
-			_putchar('A');
-			count += 2;
-			for (j = 0; j < 16; j++)
-			{
-				if (str[i] == hex[j] || str[i] == hex2[j])
-				{
-					_putchar(hex[j]);
-					count++;
-					break;
-				}
-			}
-			for (k = 0; k < 16; k++)
-			{
-				if (str[i + 1] == hex[k] || str[i + 1] == hex2[k])
-				{
-					_putchar(hex[k]);
-					count++;
-					break;
-				}
-			}
+			j = str[i] / 16;
+			_putchar(hex[j]);
+			j = str[i] % 16;
+			_putchar(hex[j]);
+			count += 4;
 		}
 		else
 		{
 			_putchar(str[i]);
 			count++;
 		}
+		i++;
 	}
-
 	return (count);
+
 }
