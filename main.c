a74264ced2e85761f2c0ec4432f5b7a9f36aa504
asimsharf <asimsharf@gmail.com>
2023-03-30 00:09:02 +0300

    Updates

--- main.c
+++ main.c
@@ -13,19 +13,18 @@ int main(void)
 	int len, len2;
 
 	len = _printf("%p\n", NULL);
-	printf("len: %d\n", len);
+	// printf("len: %d\n", len);
 
 	len2 = printf("%p\n", NULL);
-	printf("len2: %d\n", len2);
+	// printf("len2: %d\n", len2);
 
 
-
-	//fflush(stdout);
-	// if (len != len2)
-	// {
-	// 	printf("Lengths differ.\n");
-	// 	fflush(stdout);
-	// 	return (1);
-	// }
+	fflush(stdout);
+	if (len != len2)
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 	return (0);
 }
