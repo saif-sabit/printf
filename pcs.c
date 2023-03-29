a061ce67d713cb94172d8c4c4e91416a7b8b6601
asimsharf <asimsharf@gmail.com>
2023-03-29 23:05:12 +0300

    Non printable characters (0 < ASCII value < 32 or >= 127) are printed this way: \x, followed by the ASCII code value in hexadecimal (upper case - always 2 characters)

--- pcs.c
+++ pcs.c
@@ -5,36 +5,34 @@
  * @str: list of arguments
  * Return: number of characters printed
  */
+//Handle the following custom conversion specifier:
 
 int pcs(char *str)
 {
 
+
 	int i, j, k;
 	int count = 0;
 	char *hex = "0123456789abcdef";
+	char *hex2 = "0123456789ABCDEF";
 
 	if (str == NULL)
 	{
-		str = "(null)";
+		return (-1);
 	}
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 32 && str[i] <= 126)
-		{
-			_putchar(str[i]);
-			count++;
-		}
-		else
+		if (str[i] < 32 || str[i] >= 127)
 		{
 			_putchar('\\');
-			_putchar('0');
 			_putchar('x');
+			_putchar('0');
 			_putchar('A');
 			count += 2;
 			for (j = 0; j < 16; j++)
 			{
-				if (str[i] == hex[j])
+				if (str[i] == hex[j] || str[i] == hex2[j])
 				{
 					_putchar(hex[j]);
 					count++;
@@ -43,7 +41,7 @@ int pcs(char *str)
 			}
 			for (k = 0; k < 16; k++)
 			{
-				if (str[i + 1] == hex[k])
+				if (str[i + 1] == hex[k] || str[i + 1] == hex2[k])
 				{
 					_putchar(hex[k]);
 					count++;
@@ -51,7 +49,12 @@ int pcs(char *str)
 				}
 			}
 		}
+		else
+		{
+			_putchar(str[i]);
+			count++;
+		}
 	}
-	return (count);
 
+	return (count);
 }
